ab5d1b3c9b1e03d94c90b8c857bb72c437846fed
Mong Ramos Jr <mongramosjr@gmail.com>
2018-01-16 12:09:05 +0800

    Initial commit

---
+++ app/src/main/java/com/varatto/sarisaripos/PointOfSale.java
@@ -0,0 +1,13 @@
+package com.varatto.sarisaripos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PointOfSale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_point_of_sale);
+    }
+}
